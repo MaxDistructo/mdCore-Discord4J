67463ef75918a96130d521128bd63dfa29449379
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-12-08 10:45:08 -0600

    Full Theoretical File. This doesn't actually work yet

---
+++ src/main/java/maxdistructo/droidbot2/core/File.java
@@ -0,0 +1,14 @@
+package maxdistructo.droidbot2.core
+
+public class File extends java.io.File{
+
+@Override boolean exists(){
+if(java.io.file.exists())
+return true;
+}
+else{
+file.mkdirs().
+return true; 
+}
+
+}
