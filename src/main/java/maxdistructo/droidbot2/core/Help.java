b745396356fc0aae3044402f519f8bfadeb60cdc
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-05 14:45:26 -0600

    Help Command Builder based on new Server Config

---
+++ src/main/java/maxdistructo/droidbot2/core/Help.java
@@ -0,0 +1,22 @@
+package maxdistructo.droidbot2.core
+
+public class Help{
+
+public static String helpCommand(IMessage message){
+IGuild guild = message.getGuild();
+IUser user = message.getAuthor();
+JSONObject config = Config.readServerConfig(guild);
+JSONObject commandsObject = config.getJSONObject("Commands");
+JSONArray commandsJSON = commandsObject.getJSONArray("commands");
+String[] commands = Utils.toStringArray(commandsJSON);
+StringBuilder helpCommandBuilder = new StringBuilder();
+int i = 0;
+while(i < commands.length){
+helpCommandBuilder.append(commands[i] + " " + commandsObject.getJSONArray(commands[i]).getString("disc") + "\n");
+i++;
+}
+return helpCommandBuilder.toString();
+
+}
+
+}
