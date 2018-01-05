4e64af794b1f77f9ec08415da38d8aec03b9cecd
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-05 11:53:17 -0600

    Start creation of new Config Command enable/disable

---
+++ src/main/java/maxdistructo/droidbot2/core/Command.java
@@ -0,0 +1,19 @@
+package maxdistructo.droidbot2.core
+
+public class Command{
+
+public static boolean isEnabled(String commandName, IGuild guild){
+
+try{
+JSONObject config = Config.readServerConfig(guild);
+JSONObject commands = config.getJSONObject("Commands");
+JSONObject command = commands.getJSONObject(commandName);
+return command.getBoolean("enabled");
+}
+catch(Exception e){
+Message.sendError(e);
+}
+
+}
+
+}
