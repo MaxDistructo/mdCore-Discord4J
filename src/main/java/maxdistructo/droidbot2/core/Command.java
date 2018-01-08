448c0b6283f9e6a710cb51e016b8680d44f958e2
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-08 08:07:54 -0600

    Add moderator check

--- src/main/java/maxdistructo/droidbot2/core/Command.java
+++ src/main/java/maxdistructo/droidbot2/core/Command.java
@@ -16,4 +16,18 @@ Message.sendError(e);
 
 }
 
+public static boolean getModeratorStatus(String commandName, IGuild guild){
+
+try{
+JSONObject config = Config.readServerConfig(guild);
+JSONObject commands = config.getJSONObject("Commands");
+JSONObject command = commands.getJSONObject(commandName);
+return command.getBoolean("moderator");
+}
+catch(Exception e){
+Message.sendError(e);
+}
+
+}
+
 }
