f432b6ca282c1be2466c3d68edbe4b3a46d4f007
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-25 09:02:12 -0600

    Remove last one doesn't get appendage

--- src/main/java/maxdistructo/droidbot2/core/Utils.java
+++ src/main/java/maxdistructo/droidbot2/core/Utils.java
@@ -122,13 +122,8 @@ public class Utils {
         StringBuilder stringBuilder = new StringBuilder();
         int i = startAt;
         while (i < input.length) {
-            if(i - 1 == input.length) {
-                stringBuilder.append(input[i]);
-            }
-            else{
                 stringBuilder.append(input[i]);
                 stringBuilder.append("\n");
-            }
             i++;
         }
         return stringBuilder.toString();
