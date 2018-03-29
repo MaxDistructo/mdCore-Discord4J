3b9215d6cf678f4fe234b645ca82ea8a77f3d6c2
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-03-29 12:19:46 -0500

    Quick Bot Start Thing
    Will be constructed with the following pieces of data.  (Token, Prefix, DefaultListener) Will support other listeners through a register listener function.

--- src/main/java/maxdistructo/droidbot2/core/obj/IBot.kt
+++ src/main/java/maxdistructo/droidbot2/core/obj/IBot.kt
@@ -1,6 +1,14 @@
+package maxdistructo.droidbot2.core.obj
+
+import maxdistructo.droidbot2.core.obj.impl.Help
+import org.slf4j.Logger
+import sx.blah.discord.api.IDiscordClient
 
 object IBot{
-
-
-
+  
+  val help : Help
+  val prefix : String
+  val client : IDiscordClient
+  val logger : Logger
+ 
 }
