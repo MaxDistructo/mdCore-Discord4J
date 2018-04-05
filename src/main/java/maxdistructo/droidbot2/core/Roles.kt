4d00f4c7ab8a8ffd8c62089fdd17960972fc5795
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-04-05 13:17:46 -0500

    Delete my specialized code from core.

--- src/main/java/maxdistructo/droidbot2/core/Roles.kt
+++ src/main/java/maxdistructo/droidbot2/core/Roles.kt
@@ -8,63 +8,6 @@ import java.awt.*
 import java.util.EnumSet
 
 object Roles {
-    fun checkForBotAbuse(message: IMessage): Boolean {
-        val user = message.author
-        val rolesList = user.getRolesForGuild(message.guild)
-        val guildRoles = message.guild.getRolesByName("Bot Abuser")
-        if (guildRoles.isEmpty()) {
-            println("Could not find role Bot Abuser for server " + message.guild.name)
-            return false
-        }
-        val botAbuser = guildRoles[0]
-        return rolesList.contains(botAbuser)
-    }
-
-    fun checkForPayday(message: IMessage): Boolean {
-        val user = message.author
-        val rolesList = user.getRolesForGuild(message.guild)
-        val guildRoles = message.guild.getRolesByName("Payday")
-        if (guildRoles.isEmpty()) {
-            println("Could not find role Payday for server " + message.guild.name)
-            return false
-        }
-        val payday = guildRoles[0]
-        return rolesList.contains(payday)
-    }
-
-    fun applyBotAbuser(message: IMessage, mentioned: IUser) {
-        val guildRoles = message.guild.getRolesByName("Bot Abuser")
-        if (guildRoles.isEmpty()) {
-            println("Could not find role Bot Abuser for server " + message.guild.name)
-        } else {
-            val role = guildRoles[0]
-            mentioned.addRole(role)
-        }
-    }
-
-    fun applyPayday(message: IMessage, mentioned: IUser) {
-        val guild = message.guild
-        val channelList = guild.getRolesByName("Payday")
-        val role = channelList[0]
-        mentioned.addRole(role)
-    }
-
-    fun removeBotAbuser(message: IMessage, mentioned: IUser) {
-        val guildRoles = message.guild.getRolesByName("Bot Abuser")
-        if (guildRoles.isEmpty()) {
-            println("Could not find role Bot Abuser for server " + message.guild.name)
-        } else {
-            val role = guildRoles[0]
-            mentioned.removeRole(role)
-        }
-    }
-
-    fun removePayday(message: IMessage, mentioned: IUser) {
-        val guildRoles = message.guild.getRolesByName("Payday")
-        val role = guildRoles[0]
-        mentioned.removeRole(role)
-    }
-
     fun getRole(message: IMessage, role: String): IRole? {
         val roles = message.guild.getRolesByName(role)
         return if (roles.size != 0) {
