ba2fcaf31da9623459539f07e21ff4e4e0e01c39
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-05-08 14:52:13 -0500

    Update Listener.kt

--- src/main/java/maxdistructo/discord/core/testing/Listener.kt
+++ src/main/java/maxdistructo/discord/core/testing/Listener.kt
@@ -1,25 +1,26 @@
 package maxdistructo.discord.core.testing
 
 import maxdistructo.discord.core.testing.BaseBot.bot
+import maxdistructo.discord.core.message.Webhook
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent
 
 class Listener{
 
-fun onMessageRecievedEvent(event : MessageRecievedEvent){
+@EventSubscriber
+fun onMessageRecievedEvent(event : MessageReceivedEvent){
 
-val logger = bot.logger
-val message = event.message
-val guild = event.guild
-val channel = event.channel
-val user = message.author
-val contents = event.message.content.split(" ")
-
-if(user == bot.client.applicationOwner){
-  logger.info("Accepted Message: " + message.longID + " from " + user.name)
-  logger.warn("Attempting to send webhook back in channel " + channel.name)
-  Webhook.send(bot, channel, message.content)
-  logger.warn("Sent message to Discord for the webhook")
+  val logger = bot.logger
+  val message = event.message
+  val guild = event.guild
+  val channel = event.channel
+  val user = message.author
+  val contents = event.message.content.split(" ")
+  
+    if(user == bot.client.applicationOwner){
+      logger.info("Accepted Message: " + message.longID + " from " + user.name)
+      logger.warn("Attempting to send webhook back in channel " + channel.name)
+      Webhook.send(bot, channel, message.content)
+      logger.warn("Sent message to Discord for the webhook")
+    }
 }
-
-}
-
 }
