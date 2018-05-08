256d4ee75375f5e4342744fb2364ef7f78d88894
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-05-08 13:11:30 -0500

    Create BaseBot.kt

---
+++ src/main/java/maxdistructo/discord/core/testing/BaseBot.kt
@@ -0,0 +1,16 @@
+package maxdistructo.discord.core.testing
+
+lateint var bot : Bot
+lateinit var client: IDiscordClient
+lateinit var LOGGER : Logger
+
+@JvmStatic
+fun main(args : arrayOf<String>()){
+  bot = Bot(Config.readToken())
+  client = bot.client
+  LOGGER = bot.logger
+  LOGGER.info("Testing Bot loaded")
+  client.dispatcher.registerListener(Listener())
+  LOGGER.info("Registered Testing Listener")
+  
+}
