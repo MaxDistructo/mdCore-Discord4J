f95a79a72c6a4c496d60920ee99f53de693e113b
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-05-08 13:17:58 -0500

    Create Listener.kt

---
+++ src/main/java/maxdistructo/discord/core/testing/Listener.kt
@@ -0,0 +1,25 @@
+package maxdistructo.discord.core.testing
+
+import maxdistructo.discord.core.testing.BaseBot.bot
+
+class Listener{
+
+fun onMessageRecievedEvent(event : MessageRecievedEvent){
+
+val logger = bot.logger
+val message = event.message
+val guild = event.guild
+val channel = event.channel
+val user = message.author
+val contents = event.message.content.split(" ")
+
+if(user == bot.client.applicationOwner){
+  logger.info("Accepted Message: " + message.longID + " from " + user.name)
+  logger.warn("Attempting to send webhook back in channel " + channel.name)
+  Webhook.send(bot, channel, user.name, message.content)
+  logger.warn("Sent message to Discord for the webhook")
+}
+
+}
+
+}
