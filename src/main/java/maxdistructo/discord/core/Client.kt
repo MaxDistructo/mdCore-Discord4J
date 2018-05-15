8ce4d5ea7d5536681ae3df7ec7b6699013fde35a
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-05-15 15:08:18 -0500

    Fully depreciate object for non variable purposes

--- src/main/java/maxdistructo/discord/core/Client.kt
+++ src/main/java/maxdistructo/discord/core/Client.kt
@@ -1,31 +1,15 @@
 package maxdistructo.discord.core
 
-import org.slf4j.LoggerFactory
-import sx.blah.discord.api.ClientBuilder
+import org.slf4j.Logger
 import sx.blah.discord.api.IDiscordClient
-import sx.blah.discord.util.DiscordException
 
-@Deprecated("Use a Bot object instead!")
+/**
+*
+* This class is used for one purpose and that is to hold variables necessary for the rest of the core to work. This includes the client and LOGGER variables.
+*
+*/
+
 object Client {
     var client: IDiscordClient? = null
-    var prefix: String = "!"
-    val LOGGER = LoggerFactory.getLogger(Client::class.java!!)
-    fun createClient(token: String): IDiscordClient? { // Returns a new instance of the Discord client
-        val clientBuilder = ClientBuilder() // Creates the ClientBuilder instance
-        clientBuilder.withToken(token) // A to the builder
-        clientBuilder.withRecommendedShardCount()
-        try {
-            client = clientBuilder.login()
-        } catch (e: DiscordException) {
-            e.printStackTrace()
-            System.exit(0)
-        }
-
-        prefix = Config.readPrefix()
-        return client
-    }
-
-    fun registerListener(listener: Any) {
-        client!!.dispatcher.registerListener(listener)
-    }
+    var LOGGER : Logger? = null
 }
