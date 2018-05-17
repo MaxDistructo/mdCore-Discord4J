18b91750276e4ae946d5c56f96100af3d129a09e
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-05-17 11:49:35 -0500

    Update Bot.kt

--- src/main/java/maxdistructo/discord/core/impl/Bot.kt
+++ src/main/java/maxdistructo/discord/core/impl/Bot.kt
@@ -1,6 +1,7 @@
 package maxdistructo.discord.core.impl
 
 import maxdistructo.discord.core.obj.IBot
+import maxdistructo.discord.core.priv.Client
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import sx.blah.discord.api.ClientBuilder
